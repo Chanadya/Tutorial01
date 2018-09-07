ae244564e3b2eee3c4e4170a959b92cfd765c2e9
Chanadya <nanachanadya@gmail.com>
2018-09-07 14:29:10 +0700

    Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,25 @@
+public class Dosen {
+    public Dosen(String nama, String id) {
+        this.nama = nama;
+        this.id = id;
+    }
+
+    String nama;
+    String id;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
